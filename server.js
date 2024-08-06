147b5dd3606ef017a41e10aeaeda70322c7e6d18
Dixon Tan <dixon.tan14@gmail.com>
2024-08-06 18:54:46 +0800

    Add server listening port

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const http = require('http');
 const app =  require('./app');
 const { PORT } = require('/config/config')
 
-const server =  http.createServer
+const server =  http.createServer();
+
+server.listen(PORT, () => {
+    console.log(`App is running in ${process.env.NODE_ENV} on port ${PORT}`)
+})
