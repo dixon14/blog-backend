71e9725790bf655008f10127a6666b5f728ad77f
Dixon Tan <dixon.tan14@gmail.com>
2024-09-04 12:07:26 +0800

    add adminAuth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+const User = require("../models/User/User");
 const { AppErr } = require("../utils/error");
 const { getTokenFromHeader, verifyToken } = require("../utils/token")
 
@@ -23,7 +24,36 @@ const authHandler = (req, res, next) => {
     }
 }
 
+const adminAuthHandler = (req, res, next) => {
+    try {
+        //  get token from header
+        const token = getTokenFromHeader(req);
+        if (!token) {
+            throw new AppErr(401, "Token required in request header")
+        }
+        
+        // verify token
+        const decoded = verifyToken(token)
+        if (!decoded) {
+            throw new AppErr(401, "Invalid/Expired token, please login again")
+        }
+        
+        // Check if user is admin
+        User.findById(decoded.id)
+        .then(user => {
+            if (!user.isAdmin) {
+                throw new AppErr(401, "Invalid permission")
+            }
+            next();
+        })
+        .catch(err => next(err))
+    } catch (error) {
+        next(error);
+    }
+}
+
 
 module.exports = {
-    authHandler
+    authHandler,
+    adminAuthHandler
 }
