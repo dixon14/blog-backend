f53afe73d6a656e73c45198887c974700ff6a8c6
Dixon Tan <dixon.tan14@gmail.com>
2024-09-05 11:37:01 +0800

    format file

--- routes/posts/postRoutes.js
+++ routes/posts/postRoutes.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const { createPostHandler, getPostHandler, getAllPostsHandler, deletePostHandler, updatePostHandler } = require('../../controllers/posts/post_controller');
+const express = require("express");
+const {
+    createPostHandler,
+    getPostHandler,
+    getAllPostsHandler,
+    deletePostHandler,
+    updatePostHandler,
+} = require("../../controllers/posts/post_controller");
+const { authHandler } = require("../../middlewares/auth");
 
 const postRouter = express.Router();
 
-postRouter.post('', createPostHandler)
-postRouter.get('/:id',getPostHandler)
-postRouter.get('', getAllPostsHandler)
-postRouter.delete('/:id', deletePostHandler)
-postRouter.put('/:id',updatePostHandler)
+postRouter.get("/:id", getPostHandler);
+postRouter.get("", getAllPostsHandler);
+postRouter.post("", authHandler, createPostHandler);
+postRouter.delete("/:id", deletePostHandler);
+postRouter.put("/:id", updatePostHandler);
 
 module.exports = postRouter;
